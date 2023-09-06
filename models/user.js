4b2186a42d4e7d022347d9347d90f97791c25fa4
Borguuh <abdrahmanadewale@gmail.com>
2023-09-06 19:45:33 +0100

    auth done

---
+++ models/user.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 64,
+    },
+    picture: {
+      type: String,
+      default: "/avatar.png",
+    },
+    role: {
+      type: [String],
+      default: ["User"],
+      enum: ["User", "Dev", "Admin"],
+    },
+    passwordResetCode: {
+      data: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
